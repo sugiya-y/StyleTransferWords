a958dc89debb2099d229e14b318970de87894ed2
Yu Sugiyama <sugiya-y@mm.inf.uec.ac.jp>
2018-05-23 17:11:17 +0900

    replaced FLAGS

--- arbitrary_image_stylization_word/generate_style_parameter.py
+++ arbitrary_image_stylization_word/generate_style_parameter.py
@@ -34,53 +34,41 @@ import arbitrary_image_stylization_build_model as build_model
 from magenta.models.image_stylization import image_utils
 
 slim = tf.contrib.slim
+checkpoint_in = 'arbitrary_style_transfer/model.ckpt'
+output_dir = 'outputs_style'
+style_images_paths = 'images/style_images_own/*.jpg'
+content_images_paths = 'images/content_images_own/*.jpg'
+image_size = 256
+content_square_crop = False
+style_image_size = 256
+style_square_crop = False
+maximum_styles_to_evaluate = 1024
+interpolation_weights_in = '[1.0]'
 
-flags = tf.flags
-flags.DEFINE_string('checkpoint', None, 'Path to the model checkpoint.')
-flags.DEFINE_string('style_images_paths', 'images/style_images/*.jpg', 'Paths to the style images'
-                    'for evaluation.')
-flags.DEFINE_string('content_images_paths', 'images/content_images/*.jpg', 'Paths to the content images'
-                    'for evaluation.')
-flags.DEFINE_string('output_dir', 'outputs', 'Output directory.')
-flags.DEFINE_integer('image_size', 256, 'Image size.')
-flags.DEFINE_boolean('content_square_crop', False, 'Wheather to center crop'
-                     'the content image to be a square or not.')
-flags.DEFINE_integer('style_image_size', 256, 'Style image size.')
-flags.DEFINE_boolean('style_square_crop', False, 'Wheather to center crop'
-                     'the style image to be a square or not.')
-flags.DEFINE_integer('maximum_styles_to_evaluate', 1024, 'Maximum number of'
-                     'styles to evaluate.')
-flags.DEFINE_string('interpolation_weights', '[1.0]', 'List of weights'
-                    'for interpolation between the parameters of the identity'
-                    'transform and the style parameters of the style image. The'
-                    'larger the weight is the strength of stylization is more.'
-                    'Weight of 1.0 means the normal style transfer and weight'
-                    'of 0.0 means identity transform.')
-FLAGS = flags.FLAGS
 
-def styleParam(content_image_paths,style_image_paths):
+def styleParam(content_image_paths, style_image_paths):
     tf.logging.set_verbosity(tf.logging.INFO)
-    if not tf.gfile.Exists(FLAGS.output_dir):
-        tf.gfile.MkDir(FLAGS.output_dir)
+    if not tf.gfile.Exists(output_dir):
+        tf.gfile.MkDir(output_dir)
 
     with tf.Graph().as_default(), tf.Session() as sess:
         # Defines place holder for the style image.
         style_img_ph = tf.placeholder(tf.float32, shape=[None, None, 3])
-        if FLAGS.style_square_crop:
+        if style_square_crop:
             style_img_preprocessed = image_utils.center_crop_resize_image(
-                style_img_ph, FLAGS.style_image_size)
+                style_img_ph, style_image_size)
         else:
             style_img_preprocessed = image_utils.resize_image(style_img_ph,
-                                                              FLAGS.style_image_size)
+                                                              style_image_size)
 
         # Defines place holder for the content image.
         content_img_ph = tf.placeholder(tf.float32, shape=[None, None, 3])
-        if FLAGS.content_square_crop:
+        if content_square_crop:
             content_img_preprocessed = image_utils.center_crop_resize_image(
-                content_img_ph, FLAGS.image_size)
+                content_img_ph, image_size)
         else:
             content_img_preprocessed = image_utils.resize_image(
-                content_img_ph, FLAGS.image_size)
+                content_img_ph, image_size)
 
         # Defines the model.
         stylized_images, _, _, bottleneck_feat = build_model.build_model(
@@ -92,10 +80,10 @@ def styleParam(content_image_paths,style_image_paths):
             style_prediction_bottleneck=100,
             adds_losses=False)
 
-        if tf.gfile.IsDirectory(FLAGS.checkpoint):
-            checkpoint = tf.train.latest_checkpoint(FLAGS.checkpoint)
+        if tf.gfile.IsDirectory(checkpoint_in):
+            checkpoint = tf.train.latest_checkpoint(checkpoint_in)
         else:
-            checkpoint = FLAGS.checkpoint
+            checkpoint = checkpoint_in
             tf.logging.info(
                 'loading latest checkpoint file: {}'.format(checkpoint))
 
@@ -105,14 +93,14 @@ def styleParam(content_image_paths,style_image_paths):
         init_fn(sess)
 
         # Gets the list of the input style images.
-        style_img_list = tf.gfile.Glob(FLAGS.style_images_paths)
-        if len(style_img_list) > FLAGS.maximum_styles_to_evaluate:
+        style_img_list = tf.gfile.Glob(style_images_paths)
+        if len(style_img_list) > maximum_styles_to_evaluate:
             np.random.seed(1234)
             style_img_list = np.random.permutation(style_img_list)
-            style_img_list = style_img_list[:FLAGS.maximum_styles_to_evaluate]
+            style_img_list = style_img_list[:maximum_styles_to_evaluate]
 
         # Gets list of input content images.
-        content_img_list = tf.gfile.Glob(FLAGS.content_images_paths)
+        content_img_list = tf.gfile.Glob(content_images_paths)
 
         for content_i, content_img_path in enumerate(content_img_list):
             content_img_np = image_utils.load_np_image_uint8(content_img_path)[:, :, :
@@ -125,7 +113,7 @@ def styleParam(content_image_paths,style_image_paths):
                     content_img_ph: content_img_np
                 })
             image_utils.save_np_image(inp_img_croped_resized_np,
-                                      os.path.join(FLAGS.output_dir,
+                                      os.path.join(output_dir,
                                                    '%s.jpg' % (content_img_name)))
 
             # Computes bottleneck features of the style prediction network for the
@@ -134,7 +122,7 @@ def styleParam(content_image_paths,style_image_paths):
                 bottleneck_feat, feed_dict={style_img_ph: content_img_np})
 
             for style_i, style_img_path in enumerate(style_img_list):
-                if style_i > FLAGS.maximum_styles_to_evaluate:
+                if style_i > maximum_styles_to_evaluate:
                     break
                 style_img_name = os.path.basename(style_img_path)[:-4]
                 style_image_np = image_utils.load_np_image_uint8(style_img_path)[:, :, :
@@ -151,7 +139,7 @@ def styleParam(content_image_paths,style_image_paths):
                         style_img_ph: style_image_np
                     })
                 image_utils.save_np_image(style_img_croped_resized_np,
-                                          os.path.join(FLAGS.output_dir,
+                                          os.path.join(output_dir,
                                                        '%s.jpg' % (style_img_name)))
 
                 # Computes bottleneck features of the style prediction network for the
@@ -160,7 +148,7 @@ def styleParam(content_image_paths,style_image_paths):
                     bottleneck_feat, feed_dict={style_img_ph: style_image_np})
 
                 interpolation_weights = ast.literal_eval(
-                    FLAGS.interpolation_weights)
+                    interpolation_weights_in)
                 # Interpolates between the parameters of the identity transform and
                 # style parameters of the given style image.
                 for interp_i, wi in enumerate(interpolation_weights):
@@ -176,9 +164,5 @@ def styleParam(content_image_paths,style_image_paths):
                     # Saves stylized image.
                     image_utils.save_np_image(
                         stylized_image_res,
-                        os.path.join(FLAGS.output_dir, '%s_stylized_%s_%d.jpg' %
+                        os.path.join(output_dir, '%s_stylized_%s_%d.jpg' %
                                      (content_img_name, style_img_name, interp_i)))
-
-
-if __name__ == '__main__':
-    tf.app.run(main)
