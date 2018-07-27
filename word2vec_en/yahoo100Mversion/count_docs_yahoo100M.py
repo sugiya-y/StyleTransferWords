3a68165d999383ee632a67701f488d857df2ad72
sugiya-y <sugiya-y@mm.inf.uec.ac.jp>
2018-07-27 16:11:15 +0900

    adverbs searched!

--- word2vec_en/yahoo100Mversion/count_docs_yahoo100M.py
+++ word2vec_en/yahoo100Mversion/count_docs_yahoo100M.py
@@ -1,20 +1,32 @@
-import nltk
-from nltk.tag.perceptron import PerceptronTagger
 import os
 import _pickle as cPickle
+import re
+import nltk
+from nltk.tag.perceptron import PerceptronTagger
 
-if os.path.exists('text8_txt.pickle'):
-    with open('text8_txt.pickle', mode='rb') as f:
+if os.path.exists('yahoo100m_txt.pickle'):
+    with open('yahoo100m_txt.pickle', mode='rb') as f:
         print('text loading from pickle...')
         text = cPickle.load(f)
 else:
-    with open("/tmp/text8", 'r', encoding='utf-8') as f:
+    with open("/tmp/yahoo100m", 'r', encoding='utf-8') as f:
         print('text loading from row data...')
         text = f.read() 
-    with open('text8_txt.pickle', mode='wb') as f:
+    with open('yahoo100m_txt.pickle', mode='wb') as f:
         print('text to pickle...')
         cPickle.dump(text, f, protocol=-1)
 print('text loaded.')
+
+words_list = " "
+sentences = text.split('\n')
+for sentence in sentences:
+    splitted = sentence.split('\t')
+    # print(splitted[-1].encode('utf-8').decode())
+    if len(splitted) == 11:
+        predwords = re.sub('[0-9:.]', '', splitted[10])
+        labelwords = re.sub('[0-9:+-]', ' ', splitted[9])
+        words = labelwords + ' ' + predwords + ' '
+        words_list += words
 
 tagger = PerceptronTagger()
 tagset = None
@@ -29,7 +41,8 @@ words_tag = []
 #     words_tag_sent = nltk.tag._pos_tag(words_tok, tagset, tagger)
 #     words_tag.extend(words_tag_sent)
 
-words_tok = nltk.word_tokenize(text)
+words_tok = nltk.word_tokenize(words_list)
+print('text tokenized..')
 words_tag = nltk.tag._pos_tag(words_tok, tagset, tagger)
 
 print('text tokenized and tagged.')
@@ -43,11 +56,11 @@ for word_tag in words_tag:
         adv_list.append(word_tag[0] + ' ')
 
 print('text search finished. saving...')
-with open('text8_adv.txt', 'w', encoding='utf-8') as f:
+with open('yahoo100m_adv.txt', 'w', encoding='utf-8') as f:
     f.writelines(adv_list)
 
 print('saved text sorting and overwriting...')
-with open('text8_adv.txt', 'r', encoding='utf-8') as f:
+with open('yahoo100m_adv.txt', 'r', encoding='utf-8') as f:
     advs = f.read()
 advs_tok = nltk.word_tokenize(advs)
 freqadvs = nltk.FreqDist(advs_tok)
@@ -56,9 +69,9 @@ sortedadvs = sorted(freqadvs.items(), key=lambda x: -x[1])
 sortedlist = []
 sortedlist_with_num = []
 for sortedadv in sortedadvs:
-    sortedlist.append(sortedadv[0] + ' ')
+    sortedlist.append(sortedadv[0] + '\n')
     sortedlist_with_num.append(sortedadv[0] + ': ' + str(sortedadv[1]) + '\n')
-with open('text8_adv.txt', 'w', encoding='utf-8') as f:
+with open('yahoo100m_adv.txt', 'w', encoding='utf-8') as f:
     f.writelines(sortedlist)
-with open('text8_adv_num.txt', 'w', encoding='utf-8') as f:
+with open('yahoo100m_adv_num.txt', 'w', encoding='utf-8') as f:
     f.writelines(sortedlist_with_num)
