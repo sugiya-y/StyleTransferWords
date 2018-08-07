2615e5c79caf712e11066adc87d21c8e5fa059c2
sugiya-y <sugiya-y@mm.inf.uec.ac.jp>
2018-08-07 23:03:03 +0900

    sh editted for gan35

--- arbitrary_image_stylization_word/testgan_35.sh
+++ arbitrary_image_stylization_word/testgan_35.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]; then
+  echo "Needs argment of 'output_dir'" 1>&2
+  exit 1
+fi
+
 python test_pre_yahoo_gan.py -m 35 -u 0
-python test_yahoo.py --output_dir out_ours/yahoo100m_gan_epoch35
-python test_yahoo.py --output_dir out_ours/yahoo100m_gan_epoch35_colorpre --color_preserve
+python test_yahoo.py "--output_dir out_ours/"$1
+python test_yahoo.py "--output_dir out_ours/"$1"_colorpre" --color_preserve
+
