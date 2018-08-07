cd71cc0a0ac460074344a240063970bfe5f797c7
sugiya-y <sugiya-y@mm.inf.uec.ac.jp>
2018-08-07 21:56:32 +0900

    removed trash

--- arbitrary_image_stylization_word/testgan_35.sh
+++ arbitrary_image_stylization_word/testgan_35.sh
@@ -3,4 +3,3 @@
 python test_pre_yahoo_gan.py -m 35 -u 0
 python test_yahoo.py --output_dir out_ours/yahoo100m_gan_epoch35
 python test_yahoo.py --output_dir out_ours/yahoo100m_gan_epoch35_colorpre --color_preserve
-done
