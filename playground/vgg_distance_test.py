3c4635bee094a1d486ec38280c48790ca14bb3e9
sugiya-y <sugiya-y@mm.inf.uec.ac.jp>
2018-10-10 22:26:27 +0900

    add playground dir

---
+++ playground/vgg_distance_test.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import numpy as np
+# import argparse
+# from PIL import ImageFile
+from chainer import cuda, serializers, Variable
+from tinynet import wordQueryNet
+from tinynet_novgg import wordQueryNetNoVGG
+from wordparam import word2vector
+from vggparam import vggparamater
+from vggnet import VGGNet
+import pickle
+import argparse
+import cv2 as cv
+# import chainer
+# import chainer.functions as F
+
+
+vgg = VGGNet()
+serializers.load_hdf5('/tmp/VGG.model', vgg)
+serializers.load_npz(model_path, tinynet)
+mean = np.array([103.939, 116.779, 123.68])
+
+img = cv.imread(image).astype(np.float32)
+img -= mean
+img = cv.resize(img, (224, 224)).transpose((2, 0, 1))
+img = img[np.newaxis, :, :, :]
+
+if gpu >= 0:
+    cuda.get_device(gpu).use()
+    vgg.to_gpu()
+    img = cuda.cupy.asarray(img, dtype=np.float32)
+
+pred = vgg(Variable(img), None)
